c34e848f1d3c774f9e974ad01fb24e094d157774
Farrukh Ijaz <farrukh.ijaz@fuegoondemand.com>
2014-05-23 21:06:33 +0300

    Issue #1 WIP

--- Frameworks/JavaWebObjects/Sources/com/webobjects/appserver/WOPageNotFoundException.java
+++ Frameworks/JavaWebObjects/Sources/com/webobjects/appserver/WOPageNotFoundException.java
@@ -1,16 +1,32 @@
-/*
-Disclaimer of Warranties. Author(s) disclaim(s) to the fullest extent authorized by law any and all warranties, whether express or implied,
-including, without limitation, any implied warranties of title, non-infringement, enjoyment, integration, merchantability or fitness for any
-particular purpose.
-
-Until we decide on a license, see above. -rrk
-*/
-
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2014 TreasureBoat
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.webobjects.appserver;
 
-public class WOPageNotFoundException extends java.lang.RuntimeException {
+public class WOPageNotFoundException
+	extends
+	java.lang.RuntimeException {
 
-    public WOPageNotFoundException(String str) {
-        super(str);
-    }
+	private static final long serialVersionUID = 7526068152775009876l;
+
+	public WOPageNotFoundException(
+		String msg) {
+		super(msg);
+	}
 }
